d47991e24c59bfe6bd51f2a5f11f42d7bfdcaae5
Guilherme Feitosa <guilhermefeitosa@Guilhermes-MacBook-Pro.local>
2023-12-03 20:05:00 -0300

    first commit

---
+++ StockList/StockListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockListApp.swift
+//  StockList
+//
+//  Created by Guilherme Feitosa on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StockListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
