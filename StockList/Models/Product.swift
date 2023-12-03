f59a4c87e75008c0c405ea3c5de304dfdab91663
Guilherme Feitosa <guilhermefeitosa@Guilhermes-MacBook-Pro.local>
2023-12-03 20:35:32 -0300

    Creation of models structure

---
+++ StockList/Models/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  StockList
+//
+//  Created by Guilherme Feitosa on 03/12/23.
+//
+
+import Foundation
+
+struct Product {
+    let id: String?
+    let name: String?
+    let price: Double?
+    let brand: String?
+    let image: String?
+    let quantity: Int?
+}
---
+++ StockList/Models/ProductResponse.swift
@@ -0,0 +1,12 @@
+//
+//  ProductResponse.swift
+//  StockList
+//
+//  Created by Guilherme Feitosa on 03/12/23.
+//
+
+import Foundation
+
+struct ProductResponse {
+    let items: [Product]
+}
